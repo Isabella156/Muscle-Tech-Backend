30f791a82e790e83b7b2da57105310f8bba6471b
Bamboo <sc192yl@leeds.ac.uk>
2022-03-30 16:48:25 +0800

    database created

--- src/src/Main.java
+++ src/src/Main.java
@@ -10,7 +10,60 @@ public class Main {
   public static void createTable() throws Exception{
     try{
       Connection con = getConnection();
-      PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS tablename(id int NOT NULL AUTO_INCREMENT, first varchar(255), last varchar(255), PRIMARY KEY(id))");
+      String UserSql, VenueSql, OrderVenueSql, CourseSql, OrderCourseSql, Sql;
+      UserSql = "CREATE TABLE IF NOT EXISTS User(userId BIGINT NOT NULL AUTO_INCREMENT, " +
+              "userName VARCHAR(30) DEFAULT 'user', " +
+              "userPhoneNumber CHAR(11) NOT NULL, " +
+              "userPassword VARCHAR(20) NOT NULL, " +
+              "userType TINYINT DEFAULT 0, " +
+              "PRIMARY KEY(userId));\n";
+      VenueSql = "CREATE TABLE IF NOT EXISTS Venue(venueId BIGINT NOT NULL AUTO_INCREMENT, " +
+              "venueName VARCHAR(30) NOT NULL, " +
+              "venueInformation TEXT NOT NULL, " +
+              "venueType TINYINT DEFAULT 0, " +
+              "venueCapacity MEDIUMINT NOT NULL, " +
+              "venuePresentNumber MEDIUMINT DEFAULT 0, " +
+              "venuePrice SMALLINT NOT NULL, " +
+              "PRIMARY KEY(venueId));\n";
+      OrderVenueSql = "CREATE TABLE IF NOT EXISTS OrderVenue(orderVenueId BIGINT NOT NULL AUTO_INCREMENT, " +
+              "userId BIGINT NOT NULL, " +
+              "venueId BIGINT NOT NULL, " +
+              "orderVenueDate DATE NOT NULL, " + 
+              "orderVenueSession TINYINT NOT NULL, " + 
+              "orderVenueLocation SMALLINT NOT NULL, " + 
+              "orderVenueAttendance TINYINT DEFAULT 0, " +
+              "orderVenueFeedback TEXT, " +
+              "FOREIGN KEY (userId) REFERENCES User(userId), " +
+              "FOREIGN KEY (venueId) REFERENCES Venue(venueId), " + 
+              "PRIMARY KEY(orderVenueId));\n";
+      CourseSql = "CREATE TABLE IF NOT EXISTS Course(courseId BIGINT NOT NULL AUTO_INCREMENT, " +
+              "venueId BIGINT NOT NULL REFERENCES Venue(venueId), " +
+              "courseName VARCHAR(30) NOT NULL, " + 
+              "courseCoach VARCHAR(10) NOT NULL, " +
+              "courseInformation TEXT NOT NULL, " + 
+              "courseCapacity MEDIUMINT NOT NULL, " +
+              "coursePrice SMALLINT NOT NULL, " + 
+              "PRIMARY KEY(courseId));\n";
+      OrderCourseSql = "CREATE TABLE IF NOT EXISTS OrderCourse(orderCourseId BIGINT NOT NULL AUTO_INCREMENT, " +
+              "userId BIGINT NOT NULL, " +
+              "courseId BIGINT NOT NULL, " +
+              "orderCourseDate DATE NOT NULL, " +
+              "orderCourseSession TINYINT NOT NULL, " +
+              "orderCourse TINYINT NOT NULL, " +
+              "orderCourseAttendance TINYINT DEFAULT 0, " +
+              "orderCourseFeedback TEXT, " +
+              "FOREIGN KEY (userId) REFERENCES User(userId), " + 
+              "FOREIGN KEY (courseId) REFERENCES Course(courseId), " + 
+              "PRIMARY KEY(orderCourseId));\n";
+      PreparedStatement create = con.prepareStatement(UserSql);
+      create.executeUpdate();
+      create = con.prepareStatement(VenueSql);
+      create.executeUpdate();
+      create = con.prepareStatement(OrderVenueSql);
+      create.executeUpdate();
+      create = con.prepareStatement(CourseSql);
+      create.executeUpdate();
+      create = con.prepareStatement(OrderCourseSql);
       create.executeUpdate();
     }catch(Exception e){
       System.out.println(e);
